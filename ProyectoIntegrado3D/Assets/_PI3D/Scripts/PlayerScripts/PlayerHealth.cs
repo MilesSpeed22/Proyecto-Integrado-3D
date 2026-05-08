a9c084050954f29a56c10fa773d5ee1d001ad5d4
MilesSpeed22 <lorperdur@gmail.com>
2026-05-08 09:09:17 +0200

    amai

--- ProyectoIntegrado3D/Assets/_PI3D/Scripts/PlayerScripts/PlayerHealth.cs
+++ ProyectoIntegrado3D/Assets/_PI3D/Scripts/PlayerScripts/PlayerHealth.cs
@@ -5,9 +5,12 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int health;
     [SerializeField] int maxHealth = 3;
+    [SerializeField] GameObject camObject;
+    [SerializeField] CameraShake camShake;
     void Start()
     {
         health = maxHealth;
+        camShake = camObject.GetComponent<CameraShake>();
     }
     void Update()
     {
@@ -18,6 +21,8 @@ public class PlayerHealth : MonoBehaviour
         if (other.gameObject.CompareTag("Obstacle"))
         {
             health--;
+
+            StartCoroutine(camShake.Shake(0.15f, 0.2f));
         }
     }
 
